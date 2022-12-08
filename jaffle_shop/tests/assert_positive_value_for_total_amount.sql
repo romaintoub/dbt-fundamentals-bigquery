ae9a7c40a9a6e5f6533a40e428b90fcdba94c2eb
romaintoub <rom1.allwhite@gmail.com>
2022-12-08 15:26:26 -0500

    add test packages + test config

--- jaffle_shop/tests/assert_positive_value_for_total_amount.sql
+++ jaffle_shop/tests/assert_positive_value_for_total_amount.sql
@@ -1,5 +1,11 @@
 -- Refunds have a negative amount, so the total amount should always be >= 0.
 -- Therefore return records where this isn't true to make the test fail.
+
+{{
+  config(
+    store_failures = true
+    )
+}}
 select
   order_id,
 	sum(amount) as total_amount
